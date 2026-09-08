0e3d2b29fcfa99dc6b3629e05ad06e97f146eba7
adamrheaume-pdg <adamrheaume@gmail.com>
2026-09-08 00:19:52 -0400

    Corriger barre système et stabilité WebView Musique Québec

--- musique-quebec/app/src/main/java/com/musiquequebec/fixed/MainActivity.java
+++ musique-quebec/app/src/main/java/com/musiquequebec/fixed/MainActivity.java
@@ -1,8 +1,13 @@
 package com.musiquequebec.fixed;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -17,8 +22,49 @@ public class MainActivity extends Activity {
 
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setStatusBarColor(Color.rgb(11, 13, 16));
+        getWindow().setNavigationBarColor(Color.rgb(11, 13, 16));
+        if (Build.VERSION.SDK_INT >= 30) {
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.setSystemBarsAppearance(0,
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS |
+                        WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS);
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(0);
+        }
+
         webView = new WebView(this);
-        webView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        webView.setBackgroundColor(Color.rgb(11, 13, 16));
+        webView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        // Android 15 force l'affichage bord-à-bord pour targetSdk 35.
+        // On applique donc explicitement les marges sûres des barres système.
+        webView.setOnApplyWindowInsetsListener((v, insets) -> {
+            int top;
+            int bottom;
+            int left;
+            int right;
+            if (Build.VERSION.SDK_INT >= 30) {
+                android.graphics.Insets bars = insets.getInsets(WindowInsets.Type.systemBars());
+                top = bars.top;
+                bottom = bars.bottom;
+                left = bars.left;
+                right = bars.right;
+            } else {
+                top = insets.getSystemWindowInsetTop();
+                bottom = insets.getSystemWindowInsetBottom();
+                left = insets.getSystemWindowInsetLeft();
+                right = insets.getSystemWindowInsetRight();
+            }
+            v.setPadding(left, top, right, bottom);
+            return insets;
+        });
+
         WebSettings s = webView.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
@@ -27,18 +73,28 @@ public class MainActivity extends Activity {
         s.setAllowContentAccess(false);
         s.setBuiltInZoomControls(false);
         s.setSupportZoom(false);
+        s.setLoadWithOverviewMode(true);
+        s.setUseWideViewPort(true);
+        if (Build.VERSION.SDK_INT >= 21) {
+            s.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
+        }
+
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClient());
         setContentView(webView);
+        webView.requestApplyInsets();
+
         try {
             String html = readAsset("index.html");
             String logo64 = readAsset("logo.b64").replace("\n", "").replace("\r", "");
             String logoData = "data:image/webp;base64," + logo64;
             html = html.replace("__LOGO_DATA__", logoData);
-            html = html.replace("<div class=\"logo\">⚜️</div>", "<img class=\"logo\" src=\"" + logoData + "\" alt=\"Musique Québec\">");
+            html = html.replace("<div class=\"logo\">⚜️</div>",
+                    "<img class=\"logo\" src=\"" + logoData + "\" alt=\"Musique Québec\">");
             webView.loadDataWithBaseURL(APP_ORIGIN, html, "text/html", "UTF-8", APP_ORIGIN);
         } catch (Exception e) {
-            webView.loadData("<h2>Erreur de chargement</h2><pre>" + e + "</pre>", "text/html", "UTF-8");
+            webView.loadData("<h2>Erreur de chargement</h2><pre>" + e + "</pre>",
+                    "text/html", "UTF-8");
         }
     }
 
@@ -53,11 +109,19 @@ public class MainActivity extends Activity {
     }
 
     @Override public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
     }
 
     @Override protected void onDestroy() {
-        if (webView != null) webView.destroy();
+        if (webView != null) {
+            webView.stopLoading();
+            webView.loadUrl("about:blank");
+            webView.clearHistory();
+            webView.removeAllViews();
+            webView.destroy();
+            webView = null;
+        }
         super.onDestroy();
     }
 }

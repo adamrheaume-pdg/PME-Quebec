b6137d798b405ae7afd48472443e05c05f5c3236
adamrheaume-pdg <adamrheaume@gmail.com>
2026-09-11 00:53:58 -0400

    Add Social Quebec app Gradle config

---
+++ social-quebec/app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "quebec.social360"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "quebec.social360"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}

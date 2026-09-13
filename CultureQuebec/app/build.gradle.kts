42026d016a35e0e696f23fcae67938a1f7129048
adamrheaume-pdg <adamrheaume@gmail.com>
2026-09-13 18:38:50 -0400

    Bump Culture Quebec to 2.5.0

--- CultureQuebec/app/build.gradle.kts
+++ CultureQuebec/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "quebec.culture.donnees"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "2.4.0"
+        versionCode = 16
+        versionName = "2.5.0"
     }
 
     buildTypes {
